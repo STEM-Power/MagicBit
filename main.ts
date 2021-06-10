295be8f7d46e60c29ba1aff0f777db619b737bb1
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-06-10 15:15:13 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -48,7 +48,7 @@ enum ColorEffect {
     flash = 0x03
 }
 
-//% color="#EE6A50" weight=10 icon="\uf013"
+//% color="#EE6A50" weight=10 icon="\uf1b9"
 namespace magicbit {
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
